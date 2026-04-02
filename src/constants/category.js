e1e2e56edee5d9290e391dbcb54722cbfe94b0e9
LB2 <mohsenabdulrahman61@gmail.com>
2026-04-02 17:44:45 +0200

    category names

--- src/constants/category.js
+++ src/constants/category.js
@@ -1,8 +1,8 @@
 export const CATEGORIES_DATA = [
-  { id: 1, label: "طبيبات", iconName: "stethoscope" },
+  { id: 1, label: "صحه", iconName: "stethoscope" },
   { id: 2, label: "تعليم", iconName: "chalkboard-user" },
   { id: 3, label: "تصوير", iconName: "camera" },
- { id: 4, label: "صحة", iconName: "person-running" },
+ { id: 4, label: "رياضة", iconName: "person-running" },
   { id: 5, label: "اماكن", iconName: "location-dot" },
     { id: 6, label: "أعمال", iconName: "laptop-code" },
  
